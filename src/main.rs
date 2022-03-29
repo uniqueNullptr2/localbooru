b197ae7065982e678d52a4c54d3a534a3e358556
uniqueNullptr2 <philipp.classen@protonmail.com>
2022-03-29 19:41:49 +0200

    unbreak some things

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
-
 use anyhow::Result;
 use async_channel::Receiver;
 use futures::future::join_all;
@@ -11,26 +6,25 @@ use sqlx::{ Pool, Sqlite, sqlite::SqlitePoolOptions};
 use anyhow::Error;
 use tokio::fs::OpenOptions;
 use walkdir::{DirEntry, WalkDir};
-use tauri::{Manager};
+
 mod db;
 mod models;
 
-#[tauri::command]
-async fn get_search_results(state: tauri::State<'_, Pool<Sqlite>>) -> Result<Vec<SearchResult>, String>{
+
+async fn get_search_results(pool: Pool<Sqlite>) -> Result<Vec<SearchResult>, String>{
     println!("command executed yo");
-    let pool = state.inner();
     let r = SearchResult::get_by_search(&pool, 0).await.map_err(|_| "Failed to get Results".to_owned())?;
     println!("{}", r.len());
     Ok(r)
 }
 
-#[tauri::command]
-async fn search(pool: tauri::State<'_, Pool<Sqlite>>, path: String) -> Result<(), String>{
+
+async fn search(pool: Pool<Sqlite>, path: String) -> Result<(), String>{
     let (s, r) = async_channel::unbounded();
     let mut v = vec!();
     for _ in 0..8 {
         let r2 = r.clone();
-        let p2 = pool.inner().clone();
+        let p2 = pool.clone();
         v.push(tokio::spawn(async move {
             loop {
                 if r2.is_closed() && r2.is_empty() {
@@ -55,21 +49,22 @@ async fn search(pool: tauri::State<'_, Pool<Sqlite>>, path: String) -> Result<()
 }
 
 fn main() {
-    tauri::Builder::default()
-        .setup(|app| {
-            let mut path = tauri::api::path::home_dir().expect("dirs");
-            path = path.join(".localbooru");
-            tauri::async_runtime::block_on(tokio::fs::create_dir_all(&path)).expect("create dirs");
-            path = path.join("localbooru.db");
-            tauri::async_runtime::block_on(OpenOptions::new().create(true).write(true).open(&path)).expect("database path");
-            let pool = tauri::async_runtime::block_on(SqlitePoolOptions::new().connect(path.to_str().expect("tgus is bs"))).expect("Sqlite");
-            tauri::async_runtime::block_on(db::make_tables(&pool)).expect("failed to make tables");
-            app.manage(pool);
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![get_search_results, search])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    // tauri::Builder::default()
+    //     .setup(|app| {
+    //         let mut path = tauri::api::path::home_dir().expect("dirs");
+    //         path = path.join(".localbooru");
+    //         tauri::async_runtime::block_on(tokio::fs::create_dir_all(&path)).expect("create dirs");
+    //         path = path.join("localbooru.db");
+    //         tauri::async_runtime::block_on(OpenOptions::new().create(true).write(true).open(&path)).expect("database path");
+    //         let pool = tauri::async_runtime::block_on(SqlitePoolOptions::new().connect(path.to_str().expect("tgus is bs"))).expect("Sqlite");
+    //         tauri::async_runtime::block_on(db::make_tables(&pool)).expect("failed to make tables");
+    //         app.manage(pool);
+    //         Ok(())
+    //     })
+    //     .invoke_handler(tauri::generate_handler![get_search_results, search])
+    //     .run(tauri::generate_context!())
+    //     .expect("error while running tauri application");
+    println!("Hello There!")
 }
 
 fn is_image(e: &DirEntry) -> bool{
